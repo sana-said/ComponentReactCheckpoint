3cd05b3d15a01b3853762081e718cc0341c5a5e7
sana-said <sana.said.aissa@gmail.com>
2020-11-04 13:02:04 +0100

    first commit

---
+++ src/component/profile/FullName.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FullName() {
+    return (
+        <div>
+            <h1>NOM: SAID</h1>
+            <h1>PRENOM:SANA</h1>
+        </div>
+    )
+}
+
+export default FullName
---
+++ src/component/profile/ProfilPhoto.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import image from "./ImageProfile.jpg"
+
+function ProfilPhoto() {
+    return (
+        <div>
+            <img src={image} alt="image de mon profile" />
+            
+        </div>
+    )
+}
+
+export default ProfilPhoto
